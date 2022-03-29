a3db81570919fc13f5a487f01a3b77027a442d09
Kacper <66553710+kacperchrost@users.noreply.github.com>
2022-03-29 20:06:26 +0200

    sauvola

--- src/main/java/com/example/pb_zadanie3/HelloController.java
+++ src/main/java/com/example/pb_zadanie3/HelloController.java
@@ -31,9 +31,12 @@ public class HelloController {
     private ImageView niblackImage;
 
     @FXML
+    private ImageView sauvolaImage;
+
+    @FXML
     protected void onLoadButtonClick() throws IOException {
         final FileChooser fc = new FileChooser();
-        fc.setInitialDirectory(new File(System.getProperty("user.home") + "\\Pictures"));
+        fc.setInitialDirectory(new File(System.getProperty("user.home")));
         selectedFile = fc.showOpenDialog(test.getScene().getWindow());
         Image img = new Image(selectedFile.getPath());
         imageOrginal.setImage(img);
@@ -42,6 +45,7 @@ public class HelloController {
         width = (int)img.getWidth();
         height = (int)img.getHeight();
         Niblack(15,-1.5);
+        Sauvola(15, -1.5);
     }
 
     @FXML
@@ -102,6 +106,7 @@ public class HelloController {
                 average = sum / area;
 
                 NiBlack = average + k1 * standardDeviation;
+                //NiBlack = mean * (1+0.5*((standardDeviation/128)));
 
                 if(pixelA > NiBlack)
                     img.setRGB(column, row, 0xffffffff);
@@ -115,7 +120,74 @@ public class HelloController {
             niblackImage.setImage(convertToFxImage(img));
             niblackImage.setFitHeight(150);
             niblackImage.setFitWidth(200);
+    }
+
+    public void Sauvola(int window, double k1) throws IOException{
+        BufferedImage imageO = ImageIO.read(selectedFile);
+        BufferedImage img = ImageIO.read(selectedFile);
+
+        int iB, iG, iR, rgb, sum, num;
+        double area, mean, standardDeviation, average;
+
+        int pixelRgb, pixelR, pixelG, pixelB, pixelA;
+        double Sauvola;
+
+
+        for (int column = 0; column < width; column++) {
+            for (int row = 0; row < height; row++) {
+
+                iR = iG = iB = 0;
+                for(int ji = -window; ji < window; ji++){
+                    for(int jj = -window; jj < window; jj++){
+                        if((column + ji >= 0 && column + ji < width)&&(row + jj >= 0 && row + jj < height)){
+                            rgb = imageO.getRGB(column + ji, row + jj);
+                            iR += rgb & 0xff0000 >> 16;
+                            iG += rgb & 0x00ff00 >> 8;
+                            iB += rgb & 0xff;
+                        }
+                    }
+                }
+                sum = (iR + iG + iB) / 3;
+                area = (window)*(window)*4;
+                standardDeviation = 0.0;
+                mean = sum/area;
+                for(int ji = -window; ji < window; ji++){
+                    for(int jj = -window; jj < window; jj++){
+                        if(column + ji >= 0 && column + ji < width){
+                            if(row + jj >= 0 && row + jj < height){
+                                rgb = imageO.getRGB(column + ji, row + jj);
+                                iR = rgb & 0xff0000 >> 16;
+                                iG = rgb & 0x00ff00 >> 8;
+                                iB = rgb & 0xff;
+                                num = (iR + iG + iB) / 3;
+                                standardDeviation += Math.pow(num - mean, 2);
+                            }
+                        }
+                    }
+                }
+                standardDeviation = Math.sqrt(standardDeviation/area);
+                pixelRgb = img.getRGB(column, row);
+                pixelR = pixelRgb & 0xff0000 >> 16;
+                pixelG = pixelRgb & 0x00ff00 >> 8;
+                pixelB = pixelRgb & 0xff;
+                pixelA = (pixelR + pixelG + pixelB) / 3;
+                average = sum / area;
+
+                Sauvola = mean * (1+0.5*((standardDeviation/128)));
+
+                if(pixelA > Sauvola)
+                    img.setRGB(column, row, 0xffffffff);
+                else
+                    img.setRGB(column, row, 0xff000000);
+            }
         }
+
+
+
+        sauvolaImage.setImage(convertToFxImage(img));
+        sauvolaImage.setFitHeight(150);
+        sauvolaImage.setFitWidth(200);
+    }
 
     private static Image convertToFxImage(BufferedImage image) {
         WritableImage wr = null;
